591131af56d9a53c12edcaa3d8da05c2498191d4
Jessica <jessica.martin@cbsinteractive.com>
2021-05-05 16:37:33 -0400

    Work on DB

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const compression = require('compression')
 const rateLimit = require('express-rate-limit')
 const {body, check, validationResult} = require('express-validator')
 
+const MongoClient = require('mongodb').MongoClient
+const url = 'mongodb://127.0.0.1:27017'
+const dbName = 'brightmoments-db'
+let db
+
 
 
 const app = express()
@@ -35,41 +40,62 @@ app.use(compression())
 app.use(helmet())
 app.use(limiter)
 app.use(cors())
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('brightmoments-db')
+    const venice = db.collection('venice')
 
-const getGlobalScreens = (request, response) => {
-    console.log(request)
-    response.json({ status: 200, message: `All screens in the world` })
-}
+    /** FUNCTIONS **/
+    const getGlobalScreens = (request, response) => {
+        console.log(request)
+        response.json({ status: 200, message: `All screens in the world` })
+    }
 
-const getLocalScreens = (request, response) => {
-    console.log(request.body)
-    response.json({ status: 200, message: `All screens for a location` })
-}
+    const getLocalScreens = (request, response) => {
+        console.log(request.params)
+        const location = request.params.location
+        const screensCollection = db.collection('venice').find().toArray()
+            .then(results => {
+                console.log(results)
+                response.json({ status: 200, message:` screens for ${location}: ${results} `})
+            })
+            .catch(error => {
+                console.error(error) 
+                response.json({ status: 400, message:` ${error} `})
+            })
+    }
 
-const getScreen = (request, response) => {
-    console.log(request.body)
-    response.status(200).json({ status: 200, message: `Get a screen` })
-}
+    const getScreen = (request, response) => {
+        console.log(request.params)
+        response.status(200).json({ status: 200, message: `Get a screen` })
+    }
 
-const pushScreen = (request, response) => {
-    console.log(request)
-    response.status(200).json({ status: 200, message: `POSTed` })
-}
+    const pushScreen = (request, response) => {
+        console.log(request)
+        response.status(200).json({ status: 200, message: `POSTed` })
+    }
 
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Hello, world' })
-})
-app.get('/screens', getGlobalScreens)
-app.get('/screens/:location', getLocalScreens)
-app.get('/screens/:location/:id', getScreen)
-app.post('/screens/:location/:id', pushScreen)
+/** ENDPOINTS **/
+    app.get('/', (request, response) => {
+        response.json({ info: 'Hello, world' })
+        })
+    app.get('/screens', getGlobalScreens)
+    app.get('/screens/:location', getLocalScreens)
+    app.get('/screens/:location/:id', getScreen)
+    app.post('/screens/:location/:id', pushScreen)
 
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.status(404).json({status: 404, message: "There's nothing here"});
-});
+    // Handles any requests that don't match the ones above
+    app.get('*', (req,res) =>{
+        res.status(404).json({status: 404, message: "There's nothing here"});
+    });
+  })
+  .catch(error => console.error(error))
+
+
+   
 
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
-});
+})
